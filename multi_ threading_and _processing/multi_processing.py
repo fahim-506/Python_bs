411df0213cc193e176edbfd4c57911159c7f89d7
fahim-506 <fahimp506@gmail.com>
2025-10-30 11:07:47 +0530

    multi threading and multi processing in python

--- multi_ threading_and _processing/multi_processing.py
+++ multi_ threading_and _processing/multi_processing.py
@@ -1,3 +1,36 @@
-## PRocesses that run in parallel
+## Processes that run in parallel
 ### CPU-Bound Tasks-Tasks that are heavy on CPU usage (e.g., mathematical computations, data processing).
-## PArallel execution- Multiple cores of the CPU
+## Parallel execution- Multiple cores of the CPU
+
+import multiprocessing
+import time
+
+def square_number():
+    for i in range(5):
+        time.sleep(1)
+        print(f"Square : {i*i}")
+
+def cube_number():
+    for i in range(5):
+        time.sleep(1.5)
+        print(f"Cube : {i*i*i}")
+
+square_number()
+cube_number()
+
+if __name__=="__main__":
+    #create 2 processes
+    p1 = multiprocessing.Process(target=square_number)
+    p2 = multiprocessing.Process(target=cube_number)
+
+    t = time.time()
+    #start the process
+    p1.start()
+    p2.start()
+
+    #wait for the process to complete
+    p1.join()
+    p2.join()
+    finished_time = time.time()-t
+    print(finished_time)
+
