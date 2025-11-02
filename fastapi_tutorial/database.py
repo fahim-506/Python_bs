96f4a34caf0d356cb174692764791047a5c85d19
fahim-506 <fahimp506@gmail.com>
2025-11-02 17:56:15 +0530

    remove fastapi folder

--- fastapi_tutorial/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker 
-
-db_url="postgresql://postgres:12345@localhost:5432/basic_db"
-engine=create_engine(db_url)
-Sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
--- fastapi_tutorial/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel     #data validation
-
-class product(BaseModel):
-    id:int
-    name:str
-    description:str
-    price:float
-    quantity:int
